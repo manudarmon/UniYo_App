cdbda352169334a76111cfb46c41eefd3ce653fb
Emmanuel Darmon <manudarmon@gmail.com>
2016-04-07 17:45:15 +0200

    Initial Commit

---
+++ UniYo_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UniYo_App
+//
+//  Created by Emmanuel Darmon on 07/04/2016.
+//  Copyright © 2016 Emmanuel Darmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
